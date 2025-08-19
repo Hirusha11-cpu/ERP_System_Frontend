0c6bd05e0ef60a88d5c7a45e5c36b43f2eff2254
Hirusha11-cpu <developer6@appletechlabs.com>
2025-08-19 17:27:26 +0530

    updated

--- src/components/main_components/invoice/Invoice_preview.jsx
+++ src/components/main_components/invoice/Invoice_preview.jsx
@@ -26,7 +26,7 @@ import Sales from '../account_receivable/sales/Sales';
 import Account_Payable from '../account_payable/Account_Payable';
 import Actual_reconciliation from './actual_reconciliation/Actual_reconciliation';
 import Account_receivable from '../account_receivable/api/Account_receivable';
-import Role_management from '../role_management/role/role_management';
+import Role_management from '../role_management/role/Role_management';
 import Activity_management from '../role_management/activity_log/Activity_management';
 
 const Invoice_preview = ({ target }) => {
